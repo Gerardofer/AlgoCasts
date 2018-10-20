a9864f4c2f95973fbdd635956eff3363091cfca3
gerardofer <gerardojose13@gmail.com>
2018-10-20 00:17:48 -0700

    adds removeLast() and insertLast() methods

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -28,6 +28,69 @@ class LinkedList {
     }
     return counter;
   }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    if (!this.head) {
+      return null;
+    }
+
+    let node = this.head;
+
+    while (node) {
+      if (!node.next) {
+        return node;
+      }
+      node = node.next;
+    }
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  removeFirst() {
+    if (!this.head) {
+      return null;
+    }
+
+    this.head = this.head.next;
+    return this.head;
+  }
+
+  removeLast() {
+    if (!this.head) {
+      return;
+    }
+
+    if (this.head.next === null) {
+      return (this.head = null);
+    }
+
+    let node = this.head.next;
+    let previous = this.head;
+
+    while (node.next) {
+      previous = node;
+      node = node.next;
+    }
+    previous.next = null;
+  }
+
+  insertLast(data) {
+    const last = this.getLast();
+
+    if (last) {
+      //there are elements in the list
+      last.next = new Node(data);
+    } else {
+      //there aren't any nodes in the list, update the head with the new node
+      this.head = new Node(data);
+    }
+  }
 }
 
 module.exports = { Node, LinkedList };
